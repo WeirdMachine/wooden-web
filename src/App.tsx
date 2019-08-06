94f8d43044754d18595c4bfaea60779a1fabb3f2
Matthias <osthues.matthias@gmail.com>
2019-08-06 20:02:28 +0200

    add dummy test

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ class App extends React.Component {
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
                 {/*        <p className="App-intro">
-        </p>*/}
+                </p>*/}
                 <Game/>
             </div>
         );
---
+++ src/TicTacToe/Game.test.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Game from './Game';
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Game/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+    //Todo: Try this - https://www.valentinog.com/blog/testing-react/
+});
